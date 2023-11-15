fae7310462f6048b4f1d98596374d150121db324
nidhi-2619 <91557785+nidhi-2619@users.noreply.github.com>
2023-11-16 00:39:36 +0530

    tailwind initialise

--- Little_lemon/wsgi.py
+++ Little_lemon/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for Little_lemon project.
+WSGI config for little_lemon project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Little_lemon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'little_lemon.settings')
 
 application = get_wsgi_application()
